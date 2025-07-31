8e82e22daf176970ba44c385df5007486cfc7d8b
MateusdiSousa <mateus69107@gmail.com>
2025-07-31 11:20:35 -0300

    feat: adding database using docker

---
+++ src/main/java/com/mateus/encurta_link/EncurtaLinkApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.encurta_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtaLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtaLinkApplication.class, args);
+	}
+
+}
